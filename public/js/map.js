21fb3c164e9ef6d40a9f36e1faf3b230f43031a4
xinngil <jordanorrego1320@gmail.com>
2025-07-12 20:31:53 -0400

    Update map.js

--- public/js/map.js
+++ public/js/map.js
@@ -1,15 +1,92 @@
-// Mapa centrado en Chile
+// Configuración del mapa (centrado en Chile)
 const map = L.map('map').setView([-33.45694, -70.64827], 13);
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://openstreetmap.org">OpenStreetMap</a>'
+}).addTo(map);
 
-// Capas
-const localesLayer = L.layerGroup().addTo(map);
-const iconoLocal = L.icon({
-    iconUrl: 'https://cdn-icons-png.flaticon.com/512/2170/2170153.png',
-    iconSize: [30, 30]
-});
+// Capas del mapa
+const capaLocales = L.layerGroup().addTo(map);
+const capaTransporte = L.layerGroup().addTo(map);
+const capaHoteles = L.layerGroup().addTo(map);
 
-// Marcador de ejemplo (luego se reemplazará con datos de Firebase)
-L.marker([-33.45694, -70.64827], { icon: iconoLocal })
-    .addTo(localesLayer)
-    .bindPopup("<b>Cosmética Bella</b><br>Aforo: 15/20");
+// Iconos personalizados
+const iconos = {
+    local: L.icon({
+        iconUrl: 'https://cdn-icons-png.flaticon.com/512/2170/2170153.png',
+        iconSize: [32, 32]
+    }),
+    taxi: L.icon({
+        iconUrl: 'https://cdn-icons-png.flaticon.com/512/2785/2785419.png',
+        iconSize: [32, 32]
+    }),
+    hotel: L.icon({
+        iconUrl: 'https://cdn-icons-png.flaticon.com/512/2589/2589904.png',
+        iconSize: [32, 32]
+    })
+};
+
+// Función principal para actualizar marcadores desde Firebase
+function actualizarMarcadores(data) {
+    // Limpiar capas antes de actualizar
+    capaLocales.clearLayers();
+    capaTransporte.clearLayers();
+    capaHoteles.clearLayers();
+
+    // Procesar datos de Firebase
+    if (data) {
+        // Locales comerciales
+        if (data.locales) {
+            Object.keys(data.locales).forEach(id => {
+                const local = data.locales[id];
+                L.marker(local.ubicacion, { icon: iconos.local })
+                    .addTo(capaLocales)
+                    .bindPopup(`
+                        <b>${local.nombre}</b><br>
+                        Tipo: ${local.tipo}<br>
+                        Aforo: ${local.aforo || 'N/A'}<br>
+                        <small>ID: ${id}</small>
+                    `);
+            });
+        }
+
+        // Transporte (taxis/colectivos)
+        if (data.transporte) {
+            Object.keys(data.transporte).forEach(id => {
+                const vehiculo = data.transporte[id];
+                L.marker(vehiculo.ubicacion, { icon: iconos.taxi })
+                    .addTo(capaTransporte)
+                    .bindPopup(`<b>${vehiculo.tipo}</b><br>Placa: ${vehiculo.placa}`);
+            });
+        }
+
+        // Hoteles
+        if (data.hoteles) {
+            Object.keys(data.hoteles).forEach(id => {
+                const hotel = data.hoteles[id];
+                L.marker(hotel.ubicacion, { icon: iconos.hotel })
+                    .addTo(capaHoteles)
+                    .bindPopup(`
+                        <b>${hotel.nombre}</b><br>
+                        Habitaciones: ${hotel.habitaciones}<br>
+                        Tel: ${hotel.telefono}
+                    `);
+            });
+        }
+    }
+}
+
+// Hacer la función accesible globalmente (para firebase.js)
+window.actualizarMarcadores = actualizarMarcadores;
+
+// Control de capas (opcional)
+const capasBase = {
+    "OpenStreetMap": L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
+};
+
+const capasOverlay = {
+    "Locales": capaLocales,
+    "Transporte": capaTransporte,
+    "Hoteles": capaHoteles
+};
+
+L.control.layers(capasBase, capasOverlay).addTo(map);
