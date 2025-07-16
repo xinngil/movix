f10fa27c3a19e2ee574db2b5a9c71a10c586e3ff
xinngil <jordanorrego1320@gmail.com>
2025-07-16 16:38:45 -0400

    Delete xg-builder directory

--- xg-builder/README.md
+++
@@ -1,2 +0,0 @@
-# XG Builder
-Generador de sitios web y apps móviles solo con una idea. Proyecto MVP inicial para integración al ecosistema XG.
--- xg-builder/functions/generate.js
+++
@@ -1,5 +0,0 @@
-
-// Aquí irá la lógica futura de generación con IA (fase 2)
-module.exports = function generarSitio(idea) {
-    return `<html><body><h1>Sitio generado por IA</h1><p>${idea}</p></body></html>`;
-};
